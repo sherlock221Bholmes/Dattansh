4107c576774382973064427a9fb4d6fbabc8d53f
Shivam Trivedi <trivedis646@gmail.com>
2022-12-21 13:49:08 +0530

    Eigth Commit

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -6,7 +6,7 @@ var days = ('0' + Math.floor(t / (1000 * 60 * 60 * 24))).slice(-2);
 var hours = ('0' + Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60))).slice((-2));
 var minutes = ('0' + Math.floor((t % (1000 * 60 * 60)) / (1000 * 60))).slice(-2);
 var seconds = ('0' + Math.floor((t % (1000 * 60)) / 1000)).slice(-2);
-document.getElementById("demo").innerHTML = "<span>D &nbsp &nbsp &nbsp" + "H &nbsp &nbsp &nbsp" + "M &nbsp &nbsp &nbsp" + "S </span><br>" + days + "&nbsp &nbsp &nbsp" + hours + "&nbsp &nbsp &nbsp" + minutes + "&nbsp &nbsp &nbsp" + seconds;
+document.getElementById("demo").innerHTML = "<em>Days-----Hours-----Minutes-----Seconds</em><br>" + days + "&nbsp &nbsp &nbsp" + hours + "&nbsp &nbsp &nbsp" + minutes + "&nbsp &nbsp &nbsp" + seconds;
     if (t < 0) {
         clearInterval(x);
         document.getElementById("demo").innerHTML = "";
